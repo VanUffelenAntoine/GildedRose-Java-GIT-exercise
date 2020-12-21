e86d98b3e0e4e3a618234d62ab498c64ad02a8b0
Antoine Van Uffelen <antoine@mansio.be>
2020-12-21 19:49:49 +0100

    Fixed errors in TestswitchItem

--- src/test/java/com/gildedrose/TestSwitchItem.java
+++ src/test/java/com/gildedrose/TestSwitchItem.java
@@ -1,2 +1,37 @@
-package com.gildedrose;public class TestSwitchItem {
+package com.gildedrose;
+
+import com.gildedrose.core.GildedRose;
+import com.gildedrose.core.Item;
+import com.gildedrose.items.NormalItem;
+import com.gildedrose.items.SwitchItem;
+import org.junit.jupiter.api.Test;
+
+public class TestSwitchItem {
+    private Item[] items = new Item[] {new SwitchItem("+5 Dexterity Vest", 10, 20)};
+    private GildedRose app = new GildedRose(items);
+
+    @Test
+    void increaseQualitySwitch() {
+        app.calculateNextDay();
+        assert(app.items[0].sellIn == 9);
+        assert(app.items[0].quality == 21);
+        app.items[0].sellIn = 10;
+        app.items[0].quality = 50;
+        app.calculateNextDay();
+        assert(app.items[0].sellIn == 9);
+        assert(app.items[0].quality == 50);
+    }
+    @Test
+    void decreaseQualitySwitch() {
+        app.items[0].sellIn = 0;
+        app.items[0].quality = 20;
+        app.calculateNextDay();
+        assert(app.items[0].sellIn == -1);
+        assert(app.items[0].quality == 19);
+        app.items[0].sellIn = -1;
+        app.items[0].quality = 0;
+        app.calculateNextDay();
+        assert(app.items[0].sellIn ==-2);
+        assert(app.items[0].quality == 0);
+    }
 }
